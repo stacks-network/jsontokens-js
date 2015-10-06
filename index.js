0a6411245e7ce3ba5a96c0bb2b96be85a22a0df7
Ryan Shea <ryan@shea.io>
2015-10-06 19:38:05 -0400

    add ES256kClient to set of exports

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ module.exports = {
     TokenSigner: require('./lib/signer'),
     TokenVerifier: require('./lib/verifier'),
     decodeToken: require('./lib/decode'),
-    MissingParametersError: require('./lib/errors').MissingParametersError
+    MissingParametersError: require('./lib/errors').MissingParametersError,
+    ES256kClient: require('./lib/crypto-clients').es256k
 }
