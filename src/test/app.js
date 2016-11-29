7c8f0c665f20d0d9c57a618ee2eb1ae667757d90
Ryan Shea <ryan@shea.io>
2016-11-28 21:43:54 -0500

    add browserify tests w/ html and bundle files

---
+++ src/test/app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import {
+    TokenSigner, TokenVerifier, decodeToken,
+    MissingParametersError, SECP256K1Client
+} from '../index'
+
+const hash = '278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f'
+const rawPrivateKey = '278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f'
+
+console.log('hash:')
+console.log(hash)
+
+console.log('raw private key:')
+console.log(rawPrivateKey)
+
+const signature = SECP256K1Client.signHash(hash, rawPrivateKey)
+
+console.log('signature:')
+console.log(signature)
--- src/test/unitTests.js
+++ src/test/unitTests.js
@@ -5,7 +5,7 @@ import test from 'tape'
 import {
     TokenSigner, TokenVerifier, decodeToken,
     MissingParametersError, SECP256K1Client
-} from './index'
+} from '../index'
 
 const rawPrivateKey = '278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f'
 const rawPublicKey = '03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479'
